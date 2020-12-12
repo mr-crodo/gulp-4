39cb1611796eb14a0b1d905a05cc4237408c8ca6
Nasib <bduti09@gmail.com>
2020-12-13 03:26:08 +0400

    browsersync setting

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,23 @@
-const { src, dest, parallel, series, watch} = require('gulp');
+const {
+  src,
+  dest,
+  parallel,
+  series,
+  watch
+} = require('gulp');
+const browserSync = require('browser-sync').create();
+
+function browsersync() {
+  browserSync.init({
+    server: {
+      baseDir: 'app/'
+    },
+    // uvidomlenie
+    notify: false,
+    // ? esli net interneta to postav false
+    online: true
+
+  })
+}
+
+exports.browsersync = browsersync;
