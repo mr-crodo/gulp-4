cbe33cb8683734f13150236d30338571d9163908
Nasib <bduti09@gmail.com>
2020-12-13 18:33:17 +0400

    scripts

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ const {
   watch
 } = require('gulp');
 const browserSync = require('browser-sync').create();
+const concat = require('gulp-concat');
 
 function browsersync() {
   browserSync.init({
@@ -20,4 +21,18 @@ function browsersync() {
   })
 }
 
+
+function scripts() {
+  return src([
+     // kacaem jquery /npm i --save-dev jquery/
+    // podklycaem jquery
+    'node_modules/jquery/dist/jquery.min.js',
+    'app/js/app.js',
+  ])
+
+.pipe(concat('app.min.js'))
+.pipe(dest('app/js/'))
+}
+
 exports.browsersync = browsersync;
+exports.scripts = scripts;
